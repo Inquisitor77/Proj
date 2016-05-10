528d6efc1508601fe3af39d8930d3e09fed3e4a3
Jo.Mo <joaquim.morais@eleve.ensai.fr>
2016-05-10 10:53:33 +0200

    Test

--- app/src/main/java/com/example/ensai/proj/Main2Activity.java
+++ app/src/main/java/com/example/ensai/proj/Main2Activity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main2Activity extends AppCompatActivity {
-//test bis
+//test bis bis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
