55ca37308a04d5a5ec6b6e0e8699b72c206354dd
Jo.Mo <joaquim.morais@eleve.ensai.fr>
2016-05-10 12:44:27 +0200

    Coucou luc :)

--- app/src/main/java/com/example/ensai/proj/AjoutPhase1.java
+++ app/src/main/java/com/example/ensai/proj/AjoutPhase1.java
@@ -2,12 +2,27 @@ package com.example.ensai.proj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AjoutPhase1 extends AppCompatActivity {
+
+    EditText nom = null;
+    EditText descrip = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajout_phase1);
+    }
+
+    public void valider(View V){
+
+
+    }
+
+    public void date(View V){
+
+        
     }
 }
