78f10addbc4c1332dccc7144a6debee6596fb653
Jo.Mo <joaquim.morais@eleve.ensai.fr>
2016-05-10 15:26:02 +0200

    Coucou luc :)

--- app/src/main/java/com/example/ensai/proj/Acceuil.java
+++ app/src/main/java/com/example/ensai/proj/Acceuil.java
@@ -1,7 +1,9 @@
 package com.example.ensai.proj;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Acceuil extends AppCompatActivity {
 //youle
@@ -9,5 +11,10 @@ public class Acceuil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void creer (View V){
+        Intent intent = new Intent(Acceuil.this, AjoutPhase1.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/ensai/proj/AjoutPhase1.java
+++ app/src/main/java/com/example/ensai/proj/AjoutPhase1.java
@@ -1,19 +1,30 @@
 package com.example.ensai.proj;
 
+import android.app.DatePickerDialog;
+import android.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class AjoutPhase1 extends AppCompatActivity {
 
     EditText nom = null;
     EditText descrip = null;
+    EditText txtDate;
+
+    private int mYear, mMonth, mDay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajout_phase1);
+
+        txtDate = (EditText) findViewById(R.id.date);
     }
 
     public void valider(View V){
@@ -21,8 +32,27 @@ public class AjoutPhase1 extends AppCompatActivity {
 
     }
 
-    public void date(View V){
+    public void date(View v){
+        // Process to get Current Date
+        final Calendar c = Calendar.getInstance();
+         mYear = c.get(Calendar.YEAR);
+         mMonth = c.get(Calendar.MONTH);
+         mDay = c.get(Calendar.DAY_OF_MONTH);
 
-        
+        // Launch Date Picker Dialog
+        DatePickerDialog dpd = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        // Display Selected date in textbox
+                        txtDate.setText(dayOfMonth + "-"
+                                + (monthOfYear + 1) + "-" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        dpd.show();
     }
+
 }
