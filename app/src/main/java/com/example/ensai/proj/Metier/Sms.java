07cd6797231fb70cbb3178fa60a8f0e393c321ea
Luc <billieresluc@gmail.com>
2016-05-17 18:26:16 +0200

    Modifications/ajouts

--- app/src/main/java/com/example/ensai/proj/Metier/Sms.java
+++ app/src/main/java/com/example/ensai/proj/Metier/Sms.java
@@ -1,13 +1,29 @@
 package com.example.ensai.proj.Metier;
 
+import android.telephony.SmsManager;
+
+import java.util.ArrayList;
+
 /**
  * Created by ensai on 17/05/16.
  */
 public class Sms {
 
-    public static void envoiSms(String numero, String texte){
+    public static boolean envoiSms(String numero, String texte){
 
+        boolean envoi = false;
+
+            if(numero.length()>= 8){
+
+                SmsManager.getDefault().sendTextMessage(numero, null, texte, null, null);
+                envoi = true;
+
+            }
+        return envoi;
 
     }
+
+
+
 
 }
